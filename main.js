8895482dc9834973b1dd576668c984e2300cdae4
元プログラマi <99943229+moto-programmer-i@users.noreply.github.com>
2024-09-24 00:23:12 +0900

    delete exe button

--- main.js
+++ main.js
@@ -3,26 +3,34 @@
 
 import { Replacement } from "./modules/replacement.js";
 import { FileUtils } from "./modules/file-utils.js";
+import { ArrayUtils } from "./modules/array-utils.js";
 
 const PATTERN_ID = "pattern";
 const TARGET_ID = "target";
 const REPLACE_ID = "replace";
 
-document.getElementById(REPLACE_ID).addEventListener("click", main);
+document.getElementById(TARGET_ID).addEventListener("change", main);
 
 
 async function main() {
+  // exit when file not found
+  if(!existFile(PATTERN_ID) || !existFile(TARGET_ID)) {
+    return;
+  }
+
+
   try {
     const pattern = await FileUtils.readFileAsText(getFile(PATTERN_ID));
     const replacements = Replacement.readPatternTsv(pattern);
-    
     const targetFile = getFile(TARGET_ID);
     let target = await FileUtils.readFileAsText(targetFile);
 
+    // replace all (need to test large file)
     for(const replacement of replacements) {
       target = replacement.replace(target);
     }
 
+    // download replaced file
     FileUtils.saveFile(target, `replaced_${targetFile.name}`);
     
   }
@@ -41,5 +49,14 @@ async function main() {
  */
 function getFile(id) {
   return document.getElementById(id).files[0];
+}
+
+/**
+ * 
+ * @param {string} id ID in HTML
+ * @returns {File}
+ */
+function existFile(id) {
+  return ArrayUtils.isNotEmpty(document.getElementById(id).files);
 }
 
---
+++ modules/array-utils.js
@@ -0,0 +1,10 @@
+export class ArrayUtils {
+    /**
+     * 
+     * @param {Array} array 
+     * @returns 
+     */
+    static isNotEmpty(array) {
+        return array && array.length;
+    }
+}
